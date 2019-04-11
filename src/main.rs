ceb5ae85ac34275724d79f3ffb6537b5a369bb2c
Edward Cragg <drq.11235@gmail.com>
2019-04-11 10:22:51 +0100

    Add basic binary, with logging and arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,73 @@
+/*
+ * dinky
+ *
+ * URL shortening service.
+ *
+ */
+
+extern crate docopt;
+#[macro_use]
+extern crate log;
+extern crate stderrlog;
+extern crate atty;
+#[macro_use]
+extern crate serde_derive;
+
+use std::path::PathBuf;
+use docopt::Docopt;
+use stderrlog::{Timestamp, ColorChoice};
+use atty::{is, Stream};
+
+const USAGE: &str = "
+Link shortening service.
+
+Usage:
+    dinky [options] [-v...] [--db=PATH]
+
+Options:
+    -h --help       Show this help message.
+    --version       Print version.
+    -v --verbose    Show extra information.
+    -d --db=PATH    Use a sqlite database at PATH.
+    -c --conf=PATH  Use configuration file at PATH.
+    -t --timestamp  Force timestamps.
+";
+
+#[derive(Debug, Deserialize, Default)]
+pub struct Args {
+    flag_verbose: usize,
+    flag_db: Option<PathBuf>,
+    flag_conf: Option<PathBuf>,
+    flag_timestamp: bool,
+}
+
+const MIN_VERBOSITY: usize = 2;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.version(Some("0.1.0".to_string())).deserialize())
+        .unwrap_or_else(|e| e.exit());
+
+    // don't output colours or include timestamps on stderr if piped
+    let (coloured_output, mut timestamp) = if is(Stream::Stderr) {
+        (ColorChoice::Auto, Timestamp::Second)
+    } else{
+        (ColorChoice::Never, Timestamp::Off)
+    };
+
+    // force timestamp
+    if args.flag_timestamp {
+        timestamp = Timestamp::Second
+    };
+
+    // start logger
+    stderrlog::new()
+        .module(module_path!())
+        .verbosity(args.flag_verbose + MIN_VERBOSITY)
+        .timestamp(timestamp)
+        .color(coloured_output)
+        .init()
+        .unwrap();
+
+    info!("dinky starting...");
 }
