0f73378284458bfce178e72969b96e405315f1df
Ed Cragg <drq.11235@gmail.com>
2019-10-08 15:11:08 +0100

    Add connect functions for sqlite and postresql

--- src/db.rs
+++ src/db.rs
@@ -1,2 +1,21 @@
 use crate::db_models::*;
 use diesel::prelude::*;
+
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+use diesel::sqlite::SqliteConnection;
+use std::env;
+
+pub fn connect_postgres() -> PgConnection {
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url)
+        .expect(&format!("Error connecting to {}", database_url))
+}
+
+pub fn connect_sqlite() -> SqliteConnection {
+    let database_path = env::var("DATABASE_PATH")
+        .expect("DATABASE_PATH must be set");
+    SqliteConnection::establish(&database_path)
+        .expect(&format!("Error connecting to {}", database_path))
+}
