5f8340424ac56a712f0de86a5c149ef753dcc6d7
Ed Cragg <drq.11235@gmail.com>
2019-10-08 15:33:55 +0100

    Add NewUrl (insertable) structure for serde interface to diesel

--- src/db_models.rs
+++ src/db_models.rs
@@ -1,8 +1,16 @@
+use crate::db_schema::urls;
+
 #[derive(Queryable)]
-pub struct Post {
+pub struct Url {
     pub id: i32,
     pub url: String,
     pub created: String,
     pub accessed: String,
     pub hits: i32,
+}
+
+#[derive(Insertable)]
+#[table_name="urls"]
+pub struct NewUrl<'a> {
+    pub url: &'a str,
 }
